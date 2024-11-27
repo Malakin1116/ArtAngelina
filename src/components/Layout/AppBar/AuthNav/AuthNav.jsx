a184a036c43255b12046657cf8160490bfc95bf7
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-11-27 23:02:02 +0200

    1.9.2

--- src/components/Layout/AppBar/AuthNav/AuthNav.jsx
+++ src/components/Layout/AppBar/AuthNav/AuthNav.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import css from "./AuthNav.module.css";
@@ -6,18 +7,32 @@ const getNavLinkClass = ({ isActive }) =>
   clsx(css.link, isActive && css.isActive);
 
 export default function AuthNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
   return (
-    <>
-      <li className={css.li}>
-        <NavLink to="/register" className={getNavLinkClass}>
-          Registration
-        </NavLink>
-      </li>
-      <li className={css.li}>
-        <NavLink to="/login" className={getNavLinkClass}>
-          SignIn
-        </NavLink>
-      </li>
-    </>
+    <div className={css.authNav}>
+      {/* Бургер-иконка */}
+      <button className={css.burger} onClick={toggleMenu}>
+        ☰
+      </button>
+
+      {/* Навигация */}
+      <ul className={clsx(css.ul, isMenuOpen && css.menuOpen)}>
+        <li className={css.li}>
+          <NavLink to="/register" className={getNavLinkClass}>
+            SignUp
+          </NavLink>
+        </li>
+        <li className={css.li}>
+          <NavLink to="/login" className={getNavLinkClass}>
+            SignIn
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
--- src/components/Layout/AppBar/UserMenu/UserMenu.jsx
+++ src/components/Layout/AppBar/UserMenu/UserMenu.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logout } from "../../../../redux/auth/operations";
 import css from "./UserMenu.module.css";
 
