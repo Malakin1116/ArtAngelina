3cca06c62f44035b08767b329d3cf9a7373f22eb
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-11-27 23:58:40 +0200

    2.0

--- src/components/Layout/AppBar/AuthNav/AuthNav.jsx
+++ src/components/Layout/AppBar/AuthNav/AuthNav.jsx
@@ -3,36 +3,58 @@ import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import css from "./AuthNav.module.css";
 
-const getNavLinkClass = ({ isActive }) =>
-  clsx(css.link, isActive && css.isActive);
-
 export default function AuthNav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen((prevState) => !prevState);
+    setIsMenuOpen((prevState) => {
+      const isOpening = !prevState;
+      if (isOpening) {
+        document.body.classList.add("modal-open");
+      } else {
+        document.body.classList.remove("modal-open");
+      }
+      return isOpening;
+    });
   };
 
   return (
-    <div className={css.authNav}>
-      {/* Бургер-иконка */}
-      <button className={css.burger} onClick={toggleMenu}>
-        ☰
+    <>
+      <button
+        className={clsx(css.burger, isMenuOpen && css["burger-active"])}
+        onClick={toggleMenu}
+      >
+        <span className={clsx(css["burger-line"], css["burger-line-first"])} />
+        <span className={clsx(css["burger-line"], css["burger-line-second"])} />
+        <span className={clsx(css["burger-line"], css["burger-line-third"])} />
+        <span className={clsx(css["burger-line"], css["burger-line-fourth"])} />
       </button>
 
-      {/* Навигация */}
-      <ul className={clsx(css.ul, isMenuOpen && css.menuOpen)}>
-        <li className={css.li}>
-          <NavLink to="/register" className={getNavLinkClass}>
-            SignUp
-          </NavLink>
-        </li>
-        <li className={css.li}>
-          <NavLink to="/login" className={getNavLinkClass}>
-            SignIn
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+      <div
+        className={clsx(css["menu-container"], isMenuOpen && css["is-open"])}
+      >
+        <ul className={css["navigation-mobile-list"]}>
+          <li className={css["navigation-items"]}>
+            <NavLink
+              to="/login"
+              className={css["menu-box-link"]}
+              onClick={toggleMenu}
+            >
+              SignIn
+            </NavLink>
+          </li>
+
+          <li className={css["navigation-items"]}>
+            <NavLink
+              to="/register"
+              className={css["menu-box-link"]}
+              onClick={toggleMenu}
+            >
+              SignUp
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
