9b43d567bfdbe383ee083dbf24c116fca6e54ce0
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-11-27 16:31:59 +0200

    1.7.2

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -2,12 +2,12 @@ import css from "./GalleryPage.module.css";
 
 export default function GalleryPage() {
   const images = [
-    { id: 1, src: "https://via.placeholder.com/300x200", alt: "Image 1" },
-    { id: 2, src: "https://via.placeholder.com/300x200", alt: "Image 2" },
-    { id: 3, src: "https://via.placeholder.com/300x200", alt: "Image 3" },
-    { id: 4, src: "https://via.placeholder.com/300x200", alt: "Image 4" },
-    { id: 5, src: "https://via.placeholder.com/300x200", alt: "Image 5" },
-    { id: 6, src: "https://via.placeholder.com/300x200", alt: "Image 6" },
+    { id: 1, src: "/ArtGallary/Zip/art1.jpg", alt: "Image 1" },
+    { id: 2, src: "/ArtGallary/Zip/art2.jpg", alt: "Image 2" },
+    { id: 3, src: "/ArtGallary/Zip/art3.jpg", alt: "Image 3" },
+    { id: 4, src: "/ArtGallary/Zip/art4.jpg", alt: "Image 4" },
+    { id: 5, src: "/ArtGallary/Zip/art5.jpg", alt: "Image 5" },
+    { id: 6, src: "/ArtGallary/Zip/art6.jpg", alt: "Image 6" },
   ];
 
   return (
