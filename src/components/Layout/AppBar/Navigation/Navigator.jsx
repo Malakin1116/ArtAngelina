1b667a1e3fcd9924782293d5cb55519937bf846d
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2025-02-08 02:21:11 +0200

    1.1

--- src/components/Layout/AppBar/Navigation/Navigator.jsx
+++ src/components/Layout/AppBar/Navigation/Navigator.jsx
@@ -1,25 +1,25 @@
 import { NavLink } from "react-router-dom";
-
-import clsx from "clsx";
+import { FaHome, FaImages, FaShoppingCart, FaUser } from "react-icons/fa";
 import css from "./Navigation.module.css";
-
-const getNavLinkClass = ({ isActive }) =>
-  clsx(css.link, isActive && css.isActive);
 
 export default function Navigation() {
   return (
-    <>
-      <li className={css.li}>
-        <NavLink to="/" className={getNavLinkClass}>
-          Home
-        </NavLink>
-      </li>
+    <nav className={css.bottomNav}>
+      <NavLink to="/" className={css.navIcon} aria-label="Home">
+        <FaHome className={css.icon} aria-hidden="true" />
+      </NavLink>
 
-      <li className={css.li}>
-        <NavLink to="/gallery" className={getNavLinkClass}>
-          Gallery
-        </NavLink>
-      </li>
-    </>
+      <NavLink to="/catalog" className={css.navIcon} aria-label="Catalog">
+        <FaImages className={css.icon} aria-hidden="true" />
+      </NavLink>
+
+      <NavLink to="/cart" className={css.navIcon} aria-label="Cart">
+        <FaShoppingCart className={css.icon} aria-hidden="true" />
+      </NavLink>
+
+      <NavLink to="/profile" className={css.navIcon} aria-label="Profile">
+        <FaUser className={css.icon} aria-hidden="true" />
+      </NavLink>
+    </nav>
   );
 }
